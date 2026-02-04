aab4b427e81f30859f8fac468fe38afeff44e066
冯若禹 <work@vonfry.name>
2026-02-04 10:26:03 +0800

    Revert "add missing comparsion"
    This reverts commit ee436128b04ebd13ecd6d349439bac065b85e129.

--- Sources/Tachikoma/Models/Model.swift
+++ Sources/Tachikoma/Models/Model.swift
@@ -1109,8 +1109,6 @@ extension LanguageModel {
         switch (lhs, rhs) {
         case let (.openai(lhsModel), .openai(rhsModel)):
             lhsModel == rhsModel
-        case let (.wecode(lhsModel), .wecode(rhsModel)):
-            lhsModel == rhsModel
         case let (.anthropic(lhsModel), .anthropic(rhsModel)):
             lhsModel == rhsModel
         case let (.google(lhsModel), .google(rhsModel)):
